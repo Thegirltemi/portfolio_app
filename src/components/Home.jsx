4da3af63314f40c669b3587c133684e4fcef0511
thegirltemi <temialamutu7@gmail.com>
2022-06-08 14:02:39 -0700

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import React from "react";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
 const Home = () => {
   return (
-    <div name='home' className='w-ful h-screen bg-[#0a192f] pl-10'>
-        {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-          <p className='text-pink-600'>Hi, my name is</p>
-          <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Temiloluwa</h1>
-          <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front-end Developer.</h2>
-          <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a Frontend developer currently focused on building responsive and attractive 
-              user interfaces.And also hoping to become a full stack developer in a couple of years.
-          </p>
-          <div>
-              <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Project
-              <span className='group-hover:rotate-90 duration-300'>
-              <ArrowRightAltIcon className='ml-3'/>
-              </span>
-              </button>
-          </div>
+    <div name="home" className="w-ful h-screen bg-[#0a192f] pl-10">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Temiloluwa
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Front-end Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I'm a Frontend developer currently focused on building responsive and
+          attractive user interfaces.And also hoping to become a full stack
+          developer in a couple of years.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center
+           hover:bg-pink-600 hover:border-pink-600">
+            View Project
+            <span className="group-hover:rotate-90 duration-300">
+              <ArrowRightAltIcon className="ml-3" />
+            </span>
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,47 +1,94 @@
-import React from 'react'
-import movieimg from  '../assets/movie.PNG'
-import covidimg from  '../assets/covid.PNG'
-
+import React from "react";
+import movieimg from "../assets/movie.PNG";
+import covidimg from "../assets/covid.JPG";
+import spaceimg from "../assets/space.JPG";
+import intropage from "../assets/intropage.JPG";
 
 const Project = () => {
   return (
-    <div name='Project' className='bg-[#0a192f] w-full md:h-full text-gray-300'>
-       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-           <div className='pb-8'>
-               <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Projects</p>
-               <p className='py-6'>Click on the projects to view</p>
-           </div>
-           <div className='md:flex justify-between sm:grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-               <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                   <div>
-                       
-                       <div className='text-center px-10 mb-10'>
-                          <img src={movieimg} alt='' className='bg-red-900 rounded'/>
-                        
-                           <p>A streaming app used for watching movie trailers or videos uploaded on youtube.
-                               Built using Reactjs and firebase
-                           </p>
-                           <a href={'https://temi-movie-app.netlify.app'} className='bg-black px-4'>Go To Site</a>
-                       </div>
-                   </div>
-               </div>
-               <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                   <div>
-                       
-                       <div className='text-center px-10 mt-10'>
-                          <img src={covidimg} alt='' className='bg-black rounded'/>
-                        
-                           <p className=' pb-4'>A site for viewing covid-19 live stats for each of the countries affected by the virus.
-                           Built using ReactJS and Covid API 
-                           </p>
-                           <a href={'https://temi-covidtracker-app.netlify.app'} className='bg-black px-4'>Go To Site</a>
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </div>
-    </div>
-  )
-}
+    <div name="Project" className="bg-[#0a192f] w-full md:h-full text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Projects
+          </p>
+          <p className="py-6">Click on the projects to view</p>
+        </div>
+        <div className="md:flex justify-between sm:grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div>
+              <div className="text-center px-10 mb-10">
+                <img src={intropage} alt="" className="rounded" />
 
-export default Project
+                <p>
+                Intro section with dropdown navigation built using ReactJS
+                </p>
+                <a
+                  href={"https://temi-dropnavigation-page.netlify.app"}
+                  className="bg-black px-4"
+                >
+                  Go To Site
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div>
+              <div className="text-center px-10 mt-10">
+                <img src={covidimg} alt="" className=" rounded" />
+
+                <p className=" pb-4">
+                  A site for viewing covid-19 live stats for each of the
+                  countries affected by the virus. Built using ReactJS and Covid
+                  API
+                </p>
+                <a
+                  href={"https://temi-covidtracker-app.netlify.app"}
+                  className="bg-black px-4"
+                >
+                  Go To Site
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div>
+              <div className="text-center px-10 mt-10">
+                <img src={spaceimg} alt="" className="bg-black rounded" />
+
+                <p className=" pb-4">
+                  Space tourism multi-page website .Built using ReactJS
+                </p>
+                <a
+                  href={"https://temi-space-tourist-app.netlify.app"}
+                  className="bg-black px-4"
+                >
+                  Go To Site
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div>
+              <div className="text-center px-10 mt-10">
+                <img src={movieimg} alt="" className=" bg-white rounded" />
+
+                <p className=" pb-4">
+                A streaming app used for watching movie trailers or videos uploaded on youtube. Built using Reactjs and TMDB
+                </p>
+                <a
+                  href={"https://temi-movie-app.netlify.app"}
+                  className="bg-black px-4"
+                >
+                  Go To Site
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
